c4c543096b480f1019f7f4fa33eddc16707039e1
Cristina Laryea <crispad06@gmail.com>
2018-04-06 13:59:51 -0400

    projectsRouter delete works

--- data/project/projectRouter.js
+++ data/project/projectRouter.js
@@ -42,4 +42,60 @@ router.get('/:id/actions', (req, res) => {
         });
 });
 
+router.post('/', (req, res) => {
+    const projects = req.body;
+    
+    projectDb
+        .insert(projects)
+        .then(response => {
+            res.status(201).json(response);
+        })
+        .catch(error => {
+            res.status(500).json({ error: 'Error posting.' });
+        });
+});
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const update = req.body;
+
+    projectDb
+        .update(id, update)
+        .then(count => {
+            if (count > 0) {
+                projectDb
+                    .get(id)
+                    .then(updatedProjects => {
+                        res.status(200).json(updatedProjects);
+                    });
+            }else {
+                res.status(404).json({ message: 'Project with this id does not updated.' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    projectDb
+        .get(id)
+        .then(response => {
+            project = {...response[0]};
+            projectDb
+                .remove(id)
+                .then(response => {
+                    res.status(200).json(project);
+                })
+                .catch(error => {
+                    res.status(500).json(error);
+                });
+        })
+                .catch(error => {
+                    res.status(500).json(error);
+                });
+});
+
 module.exports = router;
