b1aa2eb4b4440fe78e5575fc717cd13d35589015
Cristina Laryea <crispad06@gmail.com>
2018-04-06 12:17:45 -0400

    added express

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const port = 5000;
+server.listen(port, () => console.log('API running on port 5000'));
